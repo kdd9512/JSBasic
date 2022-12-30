e512334736b12a9d0424f9fca86e38c5f268f009
kdd9512 <hyunwoo8805@gmail.com>
2022-12-30 23:41:33 +0900

    20221230 3rd commit
    예제1 : 도형과 선을 조합하여 화면에 집을 출력한다.

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const ctx = canvas.getContext("2d");
 canvas.width=800;
 canvas.height=800;
 
-ctx.moveTo(50, 50); // 시작점을 가로,세로(px) 만큼 움직인다. 이 상태로는 단순히 시작점만 지정한 것이므로 그냥 아무것도 표시되지 않을 것.
-ctx.lineTo(150, 50); // 시작 지점(가로, 세로 px)에서부터 선을 긋는다
-ctx.lineTo(150, 150); // * 선을 그은 다음 커서는 시작점을 기준으로 생성된 선 끝에 위치하게 된다.
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
-ctx.stroke(); // 선을 긋는 명령을 저장한다.
-ctx.fill();
+// ctx.moveTo(50, 50); // 시작점을 가로,세로(px) 만큼 움직인다. 이 상태로는 단순히 시작점만 지정한 것이므로 그냥 아무것도 표시되지 않을 것.
+// ctx.lineTo(150, 50); // 시작 지점(가로, 세로 px)에서부터 선을 긋는다
+// ctx.lineTo(150, 150); // * 선을 그은 다음 커서는 시작점을 기준으로 생성된 선 끝에 위치하게 된다.
+// ctx.lineTo(50, 150);
+// ctx.lineTo(50, 50);
+// ctx.stroke(); // 선을 긋는 명령을 저장한다.
+// ctx.fill();
 
 // fillRect : 직사각형을 만든다
 // ctx.fillRect(50,50,100,200);
@@ -30,3 +30,20 @@ ctx.fill();
 // // .fillStyle : .fill() 에서 사용할 색상을 변경할 수 있다.
 // ctx.fillStyle = "red";
 // ctx.fill();
+
+// 집 모양을 만들어보는 예제.
+
+// (x축,y축,넓이,높이) 를 받아 도형을 만들고 fill() 의 기본색상으로 채운다
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+
+// 선을 그을 때 두께를 결정. 단위는 px
+ctx.lineWidth = 2;
+// (x축,y축,넓이,높이) 를 받아 선을 긋는다.
+ctx.fillRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 200, 20);
+
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
+ctx.fill();
